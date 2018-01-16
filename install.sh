651eda7c0d743ac538ff70ba42eb379fb5381010
Jesse Lang <jesse@jesselang.com>
2018-01-15 18:37:21 -0600

    install.sh now works in dotfiles directory

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 SCRIPT_NAME=$(basename ${0})
-SCRIPT_DIR=$(dirname ${0})
+SCRIPT_DIR=$(dirname $PWD/$(dirname ${0}))
 
 for FILE in ${SCRIPT_DIR}/*; do
   FILE=$(basename ${FILE})
 
-  if [ ${FILE} != ${SCRIPT_NAME} -a ! -e "${HOME}/.${FILE}" ]; then
+  if [ ${FILE} != ${SCRIPT_NAME} ] \
+      && [ ${FILE} != README.md ] \
+      && [ ! -e "${HOME}/.${FILE}" ];
+  then
     echo ln -snf "${SCRIPT_DIR}/${FILE}" "${HOME}/.${FILE}"
     ln -snf "${SCRIPT_DIR}/${FILE}" "${HOME}/.${FILE}"
   fi
